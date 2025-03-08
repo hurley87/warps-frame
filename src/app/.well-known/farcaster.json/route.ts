c281982b1f3f0f9ac95c35c49b557a903b517bb3
David Hurley <dhurls99@gmail.com>
2025-03-08 15:09:09 -0500

    update image file type

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -10,11 +10,11 @@ export async function GET() {
     frame: {
       version: '1',
       name: 'Arrows',
-      iconUrl: 'https://arrows.art/splash.png',
+      iconUrl: 'https://arrows.art/splash.jpg',
       homeUrl: 'https://arrows.art',
-      imageUrl: 'https://arrows.art/splash.png',
+      imageUrl: 'https://arrows.art/splash.jpg',
       buttonTitle: 'Launch Arrows',
-      splashImageUrl: 'https://arrows.art/splash.png',
+      splashImageUrl: 'https://arrows.art/splash.jpg',
       splashBackgroundColor: '#000000',
       webhookUrl:
         'https://api.neynar.com/f/app/b5a0dc7b-a36b-4fd9-8110-ff3f79f91cb9/event',
