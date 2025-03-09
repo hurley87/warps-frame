0d580e28c936353e0b1236b849bdcc217d0349d9
David Hurley <dhurls99@gmail.com>
2025-03-09 14:25:22 -0400

    update manifest file

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -9,15 +9,14 @@ export async function GET() {
     },
     frame: {
       version: '1',
-      name: 'Arrows',
-      iconUrl: 'https://arrows.art/splash.jpg',
+      name: 'Example Frame',
+      iconUrl: 'https://arrows.art/icon.png',
       homeUrl: 'https://arrows.art',
-      imageUrl: 'https://arrows.art/splash.jpg',
-      buttonTitle: 'Launch Arrows',
-      splashImageUrl: 'https://arrows.art/splash.jpg',
-      splashBackgroundColor: '#000000',
-      webhookUrl:
-        'https://api.neynar.com/f/app/b5a0dc7b-a36b-4fd9-8110-ff3f79f91cb9/event',
+      imageUrl: 'https://arrows.art/image.png',
+      buttonTitle: 'Check this out',
+      splashImageUrl: 'https://arrows.art/splash.png',
+      splashBackgroundColor: '#eeccff',
+      webhookUrl: 'https://arrows.art/api/webhook',
     },
   };
 
