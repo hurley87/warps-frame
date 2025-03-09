fe1c7946223fa6036e4739510f872602a7aac789
David Hurley <dhurls99@gmail.com>
2025-03-09 14:37:43 -0400

    update manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,20 +3,20 @@ export async function GET() {
     accountAssociation: {
       header:
         'eyJmaWQiOjc5ODgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg1MUQ0NDZFOTNhMTcxZGQxMkY4NGI3NWE4RDFCNjgyNTVGN2MxRjgyIn0',
-      payload: 'eyJkb21haW4iOiJhcnJvd3MuYXJ0In0',
+      payload: 'eyJkb21haW4iOiJ3d3cuYXJyb3dzLmFydCJ9',
       signature:
-        'MHg4NWQ2YmZiYWQzZDdlY2EwM2IxZDllMTZmMTBmYmExZjZjOGRjNjdhODQyNmI0NTc3ZWE5OTQ5YTA2NzY5NjAxMmIwZmNmY2EzZDZmYmRjMmMyNTM5ZWJiNTU1YmM2NmI5YTlkNDllMmM1ZjA5YzQ5NzI5Njc5MzgwMzhiMjcxMjFj',
+        'MHhjOGQ3ZThjNjIzNTkzZjM2ZWM1N2UyYjgwMmI2MTY3NDlhNzRhNzM3ODk5ZTljMDgzYmM3YmNkOGM0ZjRkZGI3M2M4NjIyODhhOWQzYzExN2JlYjNmYjZjZjUwYTM2MzlkZTg4NjI2ZDQxMTBkMGQ4MjkwOWVkOGJiMDMwNWM0YjFi',
     },
     frame: {
       version: '1',
       name: 'Example Frame',
-      iconUrl: 'https://arrows.art/icon.png',
-      homeUrl: 'https://arrows.art',
-      imageUrl: 'https://arrows.art/image.png',
+      iconUrl: 'https://www.arrows.art/icon.png',
+      homeUrl: 'https://www.arrows.art',
+      imageUrl: 'https://www.arrows.art/image.png',
       buttonTitle: 'Check this out',
-      splashImageUrl: 'https://arrows.art/splash.png',
+      splashImageUrl: 'https://www.arrows.art/splash.png',
       splashBackgroundColor: '#eeccff',
-      webhookUrl: 'https://arrows.art/api/webhook',
+      webhookUrl: 'https://www.arrows.art/api/webhook',
     },
   };
 
