462c61e491aa1ce3ce2695360ffb93fa0655457d
David Hurley <dhurls99@gmail.com>
2025-04-21 20:51:30 -0400

    change manifest info

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,9 +3,9 @@ export async function GET() {
     accountAssociation: {
       header:
         'eyJmaWQiOjc5ODgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg1MUQ0NDZFOTNhMTcxZGQxMkY4NGI3NWE4RDFCNjgyNTVGN2MxRjgyIn0',
-      payload: 'eyJkb21haW4iOiJ3YXJwcy5mdW5kcyJ9',
+      payload: 'eyJkb21haW4iOiJ3YXJwcy5mdW4ifQ',
       signature:
-        'MHg4NWQ2YmZiYWQzZDdlY2EwM2IxZDllMTZmMTBmYmExZjZjOGRjNjdhODQyNmI0NTc3ZWE5OTQ5YTA2NzY5NjAxMmIwZmNmY2EzZDZmYmRjMmMyNTM5ZWJiNTU1YmM2NmI5YTlkNDllMmM1ZjA5YzQ5NzI5Njc5MzgwMzhiMjcxMjFj',
+        'MHg5Y2I0NTdiN2EyOTQyY2Q4ZTE0NDNiZTYyZjhmYmVlNGM3YmYzNzRiMDM3NzRiYzJkYmVjODE3NTcyYTNiM2M2MWUzNzY0ZWUzODRmMTZmNjkwNGRiYTI2OTc1ZTY1MTY2NTFhMTFiNDMzZWZhOTMwYzYyZWY4ZmQ2YTBkYTcxMzFi',
     },
     frame: {
       version: '1',
@@ -16,8 +16,7 @@ export async function GET() {
       buttonTitle: 'Launch Warps',
       splashImageUrl: 'https://warps.fun/splash.jpg',
       splashBackgroundColor: '#000000',
-      webhookUrl:
-        'https://api.neynar.com/f/app/b5a0dc7b-a36b-4fd9-8110-ff3f79f91cb9/event',
+      webhookUrl: 'https://warps.fun/api/webhook',
     },
   };
 
